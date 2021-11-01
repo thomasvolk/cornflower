aba34943ad0952612915f66613c1e8d4b3794e4d
Thomas Volk (fedora) <info@thomasvolk.de>
2021-11-01 21:01:38 +0100

    format

--- cornflower.rb
+++ cornflower.rb
@@ -1,32 +1,35 @@
-
 module Cornflower
-    module ClassExtensions
-        def invokes(component)
-            puts "#{self} ---> #{component}"
-        end
+
+  module ClassExtensions
+    def invokes(component)
+      puts "#{self} ---> #{component}"
     end
-    class Context
-        def initialize(*components, &init_block)
-            def new_connect_method(context)
-                Proc.new { |component| puts "[#{context}] #{self} ---> #{component}" }
-            end
-            @classExtension = Module.new
-            @classExtension.define_method(:>>, new_connect_method(self))
-            init_components(components)
-            init_block.call()
-        end
-        def init_components(components)
-            components.filter { |c| c.is_a? Module }.each { |c|
-                init_component(c)
-            }
-        end
-        def init_component(component)
-            component.extend(@classExtension)
-            components = component.constants.map { |name| component.const_get(name) }
-            init_components(components)
-        end
-        private :init_components, :init_component
+  end
+
+  class Context
+    def initialize(*components, &init_block)
+      def new_connect_method(context)
+        Proc.new { |component| puts "[#{context}] #{self} ---> #{component}" }
+      end
+      @class_extension = Module.new
+      @class_extension.define_method(:>>, new_connect_method(self))
+      init_components(components)
+      init_block.call()
     end
+
+    def init_components(components)
+      components.filter { |c| c.is_a? Module }.each { |c|
+        init_component(c)
+      }
+    end
+
+    def init_component(component)
+      component.extend(@class_extension)
+      components = component.constants.map { |name| component.const_get(name) }
+      init_components(components)
+    end
+    private :init_components, :init_component
+  end
 end
 
 
@@ -36,24 +39,24 @@ include Cornflower
 
 module AWS
 
-    module Kubernetes 
-        class OnlineShop
-        end
-        class ProductCatalogService
-        end
-        class WarehouseService
-        end
+  module Kubernetes
+    class OnlineShop
     end
+    class ProductCatalogService
+    end
+    class WarehouseService
+    end
+  end
 
-    module OrderQueue
-    end
+  module OrderQueue
+  end
 
-    class ShopDatabase
-    end
+  class ShopDatabase
+  end
 
-    class ProductDatabase
-    end
-    
+  class ProductDatabase
+  end
+
 end
 
 include AWS::Kubernetes
