271f3207a5c175ab65e85ec6c94c3a1136b977b6
Thomas Volk (kubuntu-vm) <info@thomasvolk.de>
2021-11-13 16:41:03 +0100

    add model test

---
+++ test/test_model.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+require 'minitest/autorun'
+require 'cornflower'
+
+class ModelTest < Minitest::Test
+
+  def test_sealed_nodes
+    model = Cornflower::Model.new do
+      A do
+        B do
+        end
+      end
+    end
+
+    assert_raises NoMethodError do
+      model.not_existing
+    end
+    
+    assert_equal :A, model.A.name
+    assert_raises NoMethodError do
+      model.A.B.not_existing
+    end
+
+    model.sealed = false
+    assert_equal :not_existing, model.A.not_existing.name
+    
+  end
+
+end
