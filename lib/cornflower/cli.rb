9cb80597b729cc866dcbbb0d322f26c66f4153e8
Thomas Volk (kubuntu-vm) <info@thomasvolk.de>
2021-11-14 20:25:16 +0100

    add output file option

--- lib/cornflower/cli.rb
+++ lib/cornflower/cli.rb
@@ -8,7 +8,9 @@ module Cornflower
     class App
       BANNER = "Usage: cornflower [options] inputfile"
 
-      def run           
+      def run       
+        output_filename = nil    
+        
         parser = OptionParser.new do |opts|
           opts.banner = BANNER
           opts.on('-v', '--version', 'show version') do
@@ -19,22 +21,33 @@ module Cornflower
             puts opts
             exit
           end
+          opts.on("-o", "--output FILENAME", "output filename") do |file_name|
+            output_filename = file_name
+          end
         end.parse!
             
-        inputfile = ARGV.pop
-        abort("No inputfile given!\n#{BANNER}") unless inputfile
+        input_file = ARGV.pop
+        abort("No inputfile given!\n#{BANNER}") unless input_file
       
-        model = eval File.read(inputfile)
+        if output_filename
+            File.open(output_filename, 'w') { |output_file| 
+              parse_model(input_file, output_file)
+            }
+        else
+          s = StringIO.new
+          parse_model(input_file, s)
+          puts s.string
+        end
+      end
+
+      def parse_model(input_file, out)
+        model = eval File.read(input_file)
 
         plantuml = Cornflower::Export::PlanUMLExporter.new
 
-        s = StringIO.new
-
-        s << "\n@startuml\n\n"
-        plantuml.export(model, s)
-        s << "\n@enduml\n"
-
-        puts s.string
+        out << "\n@startuml\n\n"
+        plantuml.export(model, out)
+        out << "\n@enduml\n"
       end
 
     end
